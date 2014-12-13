e75d6f9ab87680becfd6106176cc6c7430dfdfe8
jjy <jjyruby@gmail.com>
2014-12-13 14:32:48 +0800

    change file extension

--- yang.rb
+++ yang.rb
@@ -9,7 +9,7 @@ end
 
 require 'pry-nav'
 #test
-source = open("./design.yang", "r").read.each_line.to_a
+source = open("./design.ys", "r").read.each_line.to_a
 lexer = Yang::Lexer.new(source, trace_scan: false)
 syntax_tree = Yang::Parser.new.parse(lexer)
 analyzer = Yang::Analyzer.new syntax_tree
