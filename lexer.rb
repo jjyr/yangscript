c8eadfbb9404ecdcd014a7e3d5a57cf3bcec30c2
jjy <jjyruby@gmail.com>
2015-01-03 20:41:00 +0800

    refactor lexer

--- lexer.rb
+++ lexer.rb
@@ -2,24 +2,7 @@ require './lexer_helper'
 require './utils'
 
 module Yang
-  class Lexer
-    include LexerHelper
-
-    DEFAULT_OPTIONS = {trace_scan: false}.freeze
-
-    attr_reader :line_no, :token, :token_str
-
-    def initialize source, options = {}
-      options = DEFAULT_OPTIONS.merge options
-      @line = nil
-      @line_index = 0
-      @line_no = 0
-      @line_size = 0
-      @eof_flag = false
-      @trace_scan = options[:trace_scan]
-      @source = source.dup
-    end
-
+  class Lexer < LexerBase
     def reserved_lookup id
       RESERVED_WORDS[id] || :id
     end
@@ -193,31 +176,6 @@ module Yang
       end
 
       @token, @token_str = token, token_str
-    end
-
-    def get_next_char
-      if @line_size > 0 && @line_index == @line_size
-        @line_index += 1
-        '\n'
-      elsif @line_index >= @line_size
-        @line_no += 1
-        if @line = @source[@line_no - 1]
-          @line_size = @line.size
-          @line_index = 0
-          @line_index += 1
-          @line[@line_index - 1]
-        else
-          @eof_flag = true;
-          nil
-        end
-      else
-        @line_index += 1
-        @line[@line_index - 1]
-      end
-    end
-
-    def put_back_char
-      @line_index -= 1 if !@eof_flag
     end
   end
 end
--- lexer_helper.rb
+++ lexer_helper.rb
@@ -1,13 +1,63 @@
-module LexerHelper
-  def isblank c
-    c =~ /\A\s\z/
+module Yang
+  module LexerHelper
+    def isblank c
+      c =~ /\A\s\z/
+    end
+
+    def isdigit c
+      c =~ /\A\d\z/
+    end
+
+    def isalpha c
+      c =~ /\A[A-Za-z_]\z/
+    end
   end
 
-  def isdigit c
-    c =~ /\A\d\z/
-  end
+  class LexerBase
+    include LexerHelper
 
-  def isalpha c
-    c =~ /\A[A-Za-z_]\z/
+    DEFAULT_OPTIONS = {trace_scan: false}.freeze
+
+    attr_reader :line_no, :token, :token_str
+
+    def initialize source, options = {}
+      options = DEFAULT_OPTIONS.merge options
+      @line = nil
+      @line_index = 0
+      @line_no = 0
+      @line_size = 0
+      @eof_flag = false
+      @trace_scan = options[:trace_scan]
+      @source = source.dup
+    end
+
+    def next_token
+      raise NotImplementedError
+    end
+
+    def get_next_char
+      if @line_size > 0 && @line_index == @line_size
+        @line_index += 1
+        '\n'
+      elsif @line_index >= @line_size
+        @line_no += 1
+        if @line = @source[@line_no - 1]
+          @line_size = @line.size
+          @line_index = 0
+          @line_index += 1
+          @line[@line_index - 1]
+        else
+          @eof_flag = true;
+          nil
+        end
+      else
+        @line_index += 1
+        @line[@line_index - 1]
+      end
+    end
+
+    def put_back_char
+      @line_index -= 1 if !@eof_flag
+    end
   end
 end
